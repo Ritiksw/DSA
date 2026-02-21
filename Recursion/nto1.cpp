398ab50be4a8b8b173566a38ed06c09e95b8adbe
Ritik Wandale <82282307+Ritiksw@users.noreply.github.com>
2026-02-21 12:41:26 +0530

    Add recursive function to print numbers in reverse

---
+++ Recursion/nto1.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void fun(int i){
+    if(i < 1){
+        return;
+    }
+    cout << i << endl;
+    
+    fun(i-1);
+}
+
+
+int main() {
+    int n;
+    
+    cin >> n;
+    fun(n);
+}
